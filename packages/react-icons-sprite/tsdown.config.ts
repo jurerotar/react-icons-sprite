3dc329f2eda8907d51c85871522b1c9df4c5a4aa
Jure Rotar <jrotar0@gmail.com>
2026-06-29 16:11:51 +0200

    chore: bumped deps & updated peer deps (#12)

--- packages/react-icons-sprite/tsdown.config.ts
+++ packages/react-icons-sprite/tsdown.config.ts
@@ -1,5 +1,29 @@
 import { defineConfig, type UserConfig } from 'tsdown';
 
+const iconPeerDependencies = [
+  '@ant-design/icons',
+  '@carbon/icons-react',
+  '@fluentui/react-icons',
+  '@fortawesome/free-solid-svg-icons',
+  '@fortawesome/react-fontawesome',
+  '@heroicons/react',
+  '@hugeicons/core-free-icons',
+  '@hugeicons/react',
+  '@mui/icons-material',
+  '@phosphor-icons/react',
+  '@primer/octicons-react',
+  '@radix-ui/react-icons',
+  '@remixicon/react',
+  '@tabler/icons-react',
+  'devicons-react',
+  'grommet-icons',
+  'lucide-react',
+  'phosphor-react',
+  'react-bootstrap-icons',
+  'react-feather',
+  'react-icons',
+];
+
 const tsdownConfig: UserConfig = defineConfig({
   target: 'esnext',
   entry: [
@@ -13,7 +37,14 @@ const tsdownConfig: UserConfig = defineConfig({
   dts: true,
   clean: true,
   deps: {
-    neverBundle: ['react', 'react-dom/server', 'webpack', 'vite'],
+    neverBundle: [
+      ...iconPeerDependencies,
+      'react',
+      'react-dom',
+      'react-dom/server',
+      'webpack',
+      'vite',
+    ],
   },
 });
 
