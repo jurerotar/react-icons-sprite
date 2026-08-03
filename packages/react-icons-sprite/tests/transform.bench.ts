bcf9edad2a1fd3beb64dc0dd2052475e1b5cac22
Jure Rotar <jrotar0@gmail.com>
2026-08-03 16:34:24 +0200

    feat: support for rendering icons by their references

--- packages/react-icons-sprite/tests/transform.bench.ts
+++ packages/react-icons-sprite/tests/transform.bench.ts
@@ -31,8 +31,74 @@ const code = `
   );
 `;
 
+const referenceUsageBlock = Array.from({ length: 80 }, (_, i) => {
+  const set = i % 3;
+  if (set === 0) {
+    return '<Button leadingIcon={BiAlarm} trailingIcon={BiAdjust} actionIcon={BiAnchorAlt} />';
+  }
+  if (set === 1) {
+    return '<Button leadingIcon={MdHome} trailingIcon={MdSettings} actionIcon={MdFavorite} />';
+  }
+  return '<Button leadingIcon={FiActivity} trailingIcon={FiAirplay} actionIcon={FiAlertCircle} />';
+}).join('\n          ');
+
+const referenceCode = `
+  import React from 'react';
+  ${imports}
+
+  const Button = (_props: {
+    leadingIcon?: React.ComponentType<React.SVGProps<SVGSVGElement>>;
+    trailingIcon?: React.ComponentType<React.SVGProps<SVGSVGElement>>;
+    actionIcon?: React.ComponentType<React.SVGProps<SVGSVGElement>>;
+  }) => null;
+
+  export const BenchmarkComponent = () => (
+    <div>
+      ${referenceUsageBlock}
+    </div>
+  );
+`;
+
+const mixedUsageBlock = Array.from({ length: 80 }, (_, i) => {
+  const set = i % 3;
+  if (set === 0) {
+    return '<Button leadingIcon={BiAlarm}><BiAdjust className="x" /><BiAnchorAlt /></Button>';
+  }
+  if (set === 1) {
+    return '<Button leadingIcon={MdHome}><MdSettings /><MdFavorite className="fav" /></Button>';
+  }
+  return '<Button leadingIcon={FiActivity}><FiAirplay /><FiAlertCircle /></Button>';
+}).join('\n          ');
+
+const mixedCode = `
+  import React from 'react';
+  ${imports}
+
+  const Button = (_props: {
+    leadingIcon?: React.ComponentType<React.SVGProps<SVGSVGElement>>;
+    children?: React.ReactNode;
+  }) => null;
+
+  export const BenchmarkComponent = () => (
+    <div>
+      ${mixedUsageBlock}
+    </div>
+  );
+`;
+
 describe('transformModule benchmark', () => {
   bench('transforms a component with many icon usages', () => {
     transformModule(code, id, () => {});
   });
+
+  bench('transforms a component with many reference prop icon usages', () => {
+    transformModule(referenceCode, id, () => {});
+  });
+
+  bench(
+    'transforms a component with mixed component and reference icon usages',
+    () => {
+      transformModule(mixedCode, id, () => {});
+    },
+  );
 });
